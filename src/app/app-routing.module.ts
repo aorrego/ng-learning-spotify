6468d00924e61d36b26d6e195184d8bf5f437bf3
Alejandro Orrego <alejandro.orrego@crystaldev.cl>
2024-11-12 12:16:22 -0300

    cambios spotify

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExampleComponent } from './example/example.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', // localhost:4200/
+    loadChildren: 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
